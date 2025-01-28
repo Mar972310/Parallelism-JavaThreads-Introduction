44bedba551fc9664ff81c739beca24c7604304aa
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2025-01-28 08:55:24 -0500

    Parte 1

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -4,11 +4,38 @@
  * and open the template in the editor.
  */
 package edu.eci.arsw.threads;
+import java.lang.Thread;
+
 
 /**
  *
- * @author hcadavid
+ * @author Team 2 (Samuel, Juan, Valentina)
  */
-public class CountThread {
-    
+public class CountThread extends Thread {
+
+    private int A;
+    private int B;
+
+    public CountThread(int A, int B){
+        this.A = A;
+        this.B = B;
+    }
+
+    public void run(){
+        printed();
+    }
+
+    public int getA(){
+        return A;
+    }
+
+    public int getB(){
+        return B;
+    }
+
+    public void printed(){
+        for (int i = getA();  i<= getB();i++ ){
+            System.out.println(i);
+        }
+    }   
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -7,12 +7,18 @@ package edu.eci.arsw.threads;
 
 /**
  *
- * @author hcadavid
+ * @author Team 2 (Samuel, Juan, Valentina)
  */
 public class CountThreadsMain {
     
     public static void main(String a[]){
+        CountThread countThread1 = new CountThread(0, 99);
+        CountThread countThread2 = new CountThread(99, 199);
+        CountThread countThread3= new CountThread(199, 299);
         
+        countThread1.run();
+        countThread2.run();
+        countThread3.run();
     }
     
 }
